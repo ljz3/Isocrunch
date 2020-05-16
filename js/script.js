b2599dff12c7f88614de5e1fbc775d5e900addda
ljz3 <kevin.ottawa@hotmail.com>
2020-05-16 19:38:11 -0400

    Moved timer to scriptjs

--- js/script.js
+++ js/script.js
@@ -234,3 +234,28 @@ function makeList(listData) {
         listElement.appendChild(listItem);
     }
 }
+
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
