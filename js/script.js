9305183132aa7f51d36b2ee25c3147ea2f1154ec
Shoeyuz <michael.shlega@gmail.com>
2020-05-15 21:38:53 -0400

    Bug Fix
    Editing of code to make sure the findExercise is able to work with lower than 1 difficulties.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
 
 //initialization of arrays storing all the workouts able to be used
 
-var abWorkouts = [["Plank Hops", 1, false,null], ["Pushup position plan", 1, false,null], 
+var abWorkouts = [["Plank Hops", 1, true,null], ["Pushup Position Plank", 1, false,null], 
 ["Situps",2, true, null],["Lower Plank",2,false, "Partial Plank"],["Crunches",2,true, null],
 ["Mountain Climbers",3,false,null], ["V Ups",3,true,null],["Plank Raise Tap Crunch", 4, false, "Plank"], 
 ["Shoulder Tap Pushups",4,true, "Shoulder Tap Knee Pushups"],["Lateral Plank Walks",4,true, "Plank"], 
 ["Jackhammers",5,true, "Situp"]];
 
-var upperChestWorkouts = [["Tension Pushup Hold",2,false, null],["Pushups",3,true, "Knee Pushups"],
+var upperChestWorkouts = [["Tension Pushup Hold",1,false, null],["Isometric Chest Push",1,true,null],["Pushups",3,true, "Knee Pushups"],
 ["Shoulder Tap Pushups",4,true, "Shoulder Tap Knee Pushups"],
 ["Burpees",4,true, "Burpees With Knee Pushups"],["Decline Pushups", 5, true, "Knee Decline Pushups"], 
 ["Clap Pushups",6,true, "Clap Knee Pushups"],
 ["Archer Pushups", 8, true, "Knee Archer Pushups"]];
 
-var lowerChestWorkouts = [ ["Incline Pushup", 2, true, "Knee Incline Pushup"],
+var lowerChestWorkouts = [ ["Incline Tension Pushup Hold",1,false, null], ["Isometric Chest Push",1,true,null],
+["Incline Pushup", 2, true, "Knee Incline Pushup"],
 ["Pseudo Pushups",5,true, "Knee Pseudo Pushups"], ["Incline Pushup Hold",2,false]];
 
 
-var tricepWorkouts = [["Incline Pushup", 2, true, "Knee Incline Pushup"],["Tricep Dips",3,false,null]
+var tricepWorkouts = [["Incline Tension Pushup Hold",1,false, null],["Incline Pushup", 1, true, "Knee Incline Pushup"],["Tricep Dips",3,false,null]
 ["Pushups",3,true, "Knee Pushups"],
 ["Shoulder Tap Pushups",4,true, "Shoulder Tap Knee Pushups"],["Burpees",4,true, "Burpees With Knee Pushups"],
 ["Lateral Plank Walks",4,true, "Plank"],["Clap Pushups",6,true, "Clap Knee Pushups"]];
 
-var calveWorkouts = [["High Knees",1,false,null], ["Calf Raises",1,false,null], ["Jump Squats",3,true,null]];
+var calveWorkouts = [["High Knees",1,true,null], ["Calf Raises",1,false,null], ["Jump Squats",3,true,null]];
 
-var quadWorkouts = [["High Knees",1,false,null], ["Wall sit",2,false,null], ["Mountain Climbers",3,false,null],
+var quadWorkouts = [["High Knees",1,true,null], ["Wall sit",1,false,null], ["Mountain Climbers",3,false,null],
 ["Burpees",4,true,"Burpee With Knee Pushup",null]["One Legged Squat",7,true,"Squat"], ["Pistol Squat",9,true,"Squat"]];
 
-var hamWorkouts = [["Hip Bridges",1,false,null], ["Mountain Climbers",3,false,null], ["Jump Squats",3,true,null],
+var hamWorkouts = [["Hip Bridges",1,false,null], ["Mountain Climbers",1,true,null], ["Jump Squats",3,true,null],
 ["One Legged Squat",7,true,"Squat"], ["Pistol Squat",9,true,"Squat"]];
 
-var upperBackWorkouts = [["Diving Dolphin",3,false,null], ["Back Widows",7,true,"Reverse Snow Angels"]];
+var upperBackWorkouts = [["Lat Pulldown With Towel", 1, true, null],["Arm Swings", 1, false, null],["Diving Dolphin",3,false,null], ["Back Widows",7,true,"Reverse Snow Angels"]];
 
-var upperBackWorkouts = [["Hip Bridges",1,false,null], ["Reverse Snow Angels",2,true,null],["Reverse Superman",2,false,null], ["Superman",3,false,null]];
+var lowerBackWorkouts = [["Hip Bridges",1,true,null], ["Goodmornings",1,false,null],["Reverse Snow Angels",2,true,null],["Reverse Superman",2,false,null], ["Superman",3,false,null]];
 
-var shoulderWorkouts = [["Shoulder Press on Floor",2,true,null], ["Diving dolphin",3,false,null], ["Plank Raise Tap Crunch",4,false,"Plank"],
+var shoulderWorkouts = [["Arm Lift",1,true,null],["Jumping Jacks",1,false,null],["Shoulder Press on Floor",2,true,null], ["Diving dolphin",3,false,null], ["Plank Raise Tap Crunch",4,false,"Plank"],
 ["Shoulder Tap Pushups",4,true,"Shoulder Tap Knee Pushups"], ["Lateral Plank Walks",4,true,"Plank"], ["Decline Pushup",5,true,"Knee Decline Pushup"],
 ["Archer Pushups",8,true,"Knee Archer Pushups"]];
 
@@ -49,7 +50,7 @@ function findExercises(muscleGroup,diff,on){
 
     var exercise = null; //storage of variable being returned
 
-    
+    var flag = false;
     do{
         exercise = muscleGroup[Math.floor(Math.random()*muscleGroup.length)]; //random workout 
         
@@ -59,6 +60,7 @@ function findExercises(muscleGroup,diff,on){
         }
         else
             flag = false;
+
     }while(flag == false)
 
     return exercise;  
