f108371fc54af2d7854dddb5e9b621180d1ac801
Shoeyuz <michael.shlega@gmail.com>
2020-05-15 20:29:29 -0400

    Update script.js
    Added arrays for abs,upperChest,lowerChest,triceps.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+//initialization of arrays storing all the workouts able to be used
+
+var abWorkouts = [["Plank Hops", 1, false,null], ["Pushup position plan", 1, false,null], 
+["Situps",2, true, null],["Lower Plank",2,false, "Partial Plank"],["Crunches",2,true, null],
+["Mountain Climbers",3,false,null], ["V Ups",3,true,null],["Plank Raise Tap Crunch", 4, false, "Plank"], 
+["Shoulder Tap Pushups",4,true, "Shoulder Tap Knee Pushups"],["Lateral Plank Walks",4,true, "Plank"], 
+["Jackhammers",5,true, "Situp"]];
+
+var upperChestWorkouts = [["Tension Pushup Hold",2,false, null],["Pushups",3,true, "Knee Pushups"],
+["Shoulder Tap Pushups",4,true, "Shoulder Tap Knee Pushups"],
+["Burpees",4,true, "Burpees With Knee Pushups"],["Decline Pushups", 5, true, "Knee Decline Pushups"], 
+["Clap Pushups",6,true, "Clap Knee Pushups"],
+["Archer Pushups", 8, true, "Knee Archer Pushups"]];
+
+var lowerChestWorkouts = [ ["Incline Pushup", 2, true, "Knee Incline Pushup"],
+["Pseudo Pushups",5,true, "Knee Pseudo Pushups"], ["Incline Pushup Hold",2,false]];
+
+
+var tricepWorkouts = [["Incline Pushup", 2, true, "Knee Incline Pushup"],["Tricep Dips",3,false,null]
+["Pushups",3,true, "Knee Pushups"],
+["Shoulder Tap Pushups",4,true, "Shoulder Tap Knee Pushups"],["Burpees",4,true, "Burpees With Knee Pushups"],
+["Lateral Plank Walks",4,true, "Plank"],["Clap Pushups",6,true, "Clap Knee Pushups"]];
